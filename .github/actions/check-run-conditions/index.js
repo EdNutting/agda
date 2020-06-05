65406331bc0439951df59ee7931f9dc45074cdca
Ed Nutting <ed@beyondrisc.com>
2020-06-05 16:49:53 +0100

    Get pull requests

--- .github/actions/check-run-conditions/index.js
+++ .github/actions/check-run-conditions/index.js
@@ -1,20 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function log(data) {
+  console.log(JSON.stringify(data, null, "  "));
+}
+
 async function run() {
   try { 
     const repoToken = core.getInput('repo-token', { required: true });
 
-    console.log(JSON.stringify(github.context, null, "  "));
-    console.log(Object.keys(github));
+    // log (github.context);
 
-    const client = new github.GitHub(repoToken);
-    // await client.pulls.get({
-    //   owner: github.context.repository.owner.login,
-    //   repo: github.context.repository.name,
-    //   state: "open",
-    //   base: github.context.ref
-    // });
+    const client = new github.getOctokit(repoToken);
+    let pullRequests = await client.pulls.get({
+      owner: github.context.repository.owner.login,
+      repo: github.context.repository.name,
+      state: "open",
+      base: github.context.ref
+    });
+
+    log(pullRequests);
   }
   catch (error) {
     core.setFailed(error.message);
