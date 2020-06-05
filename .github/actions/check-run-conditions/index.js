b01bb3077c3cb2d20d1e8f5838d26c49acf5ef37
Ed Nutting <ed@beyondrisc.com>
2020-06-05 16:42:30 +0100

    Log github keys

--- .github/actions/check-run-conditions/index.js
+++ .github/actions/check-run-conditions/index.js
@@ -6,13 +6,14 @@ async function run() {
     const repoToken = core.getInput('repo-token', { required: true });
 
     console.log(JSON.stringify(github.context, null, "  "));
+    console.log(Object.keys(github));
 
     const client = new github.GitHub(repoToken);
     // await client.pulls.get({
     //   owner: github.context.repository.owner.login,
     //   repo: github.context.repository.name,
     //   state: "open",
-    //   base: github.context.
+    //   base: github.context.ref
     // });
   }
   catch (error) {
